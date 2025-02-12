14db8db33c0b17f00963891ae7cc090dea013444
JSPV220403 <162592820+JSPV220403@users.noreply.github.com>
2025-02-12 12:41:58 +0530

    Update db.js

--- server/versions/v1/models/db.js
+++ server/versions/v1/models/db.js
@@ -4,13 +4,13 @@ dotenv.config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD,
+  people,
+  root, 
+  jspv,
    {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT, 
+  host: localhost,
+  dialect: mysql,
+  port: 3306, 
   logging: false,
    }
 );
